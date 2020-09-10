f9a01059298ac5599e3f376d36d177c6aeaa7934
Nuno Pereira <kroglice@users.noreply.github.com>
2020-09-10 18:12:33 +0100

    Update

--- zsh.sh
+++ zsh.sh
@@ -3,3 +3,9 @@
 sudo apt update
 sudo apt install curl zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# .zshrc aliases
+echo 'alias ..="cd .."' >> ~/.zshrc
+echo 'alias lt="ls -alt"' >> ~/.zshrc
+echo 'alias nvidia="watch -n 1 nvidia-smi"' >> ~/.zshrc
+echo 'alias sensors="watch -n 1 sensors"' >> ~/.zshrc
